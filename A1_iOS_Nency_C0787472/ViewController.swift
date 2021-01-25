96dd9dcba2f462c61704908991cafe90dd0f9b79
Nency <nency@Nencys-Mac.local>
2021-01-25 13:08:54 -0500

    init commit

---
+++ A1_iOS_Nency_C0787472/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A1_iOS_Nency_C0787472
+//
+//  Created by Nency on 2021-01-25.
+//  Copyright © 2021 Nency. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ A1_iOS_Nency_C0787472Tests/A1_iOS_Nency_C0787472Tests.swift
@@ -0,0 +1,34 @@
+//
+//  A1_iOS_Nency_C0787472Tests.swift
+//  A1_iOS_Nency_C0787472Tests
+//
+//  Created by Nency on 2021-01-25.
+//  Copyright © 2021 Nency. All rights reserved.
+//
+
+import XCTest
+@testable import A1_iOS_Nency_C0787472
+
+class A1_iOS_Nency_C0787472Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
